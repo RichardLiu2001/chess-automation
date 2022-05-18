f84dc8e7fc211ce6a0bf03534c8855d27934ba93
Richard Liu <richard.liu.tx@gmail.com>
2022-05-18 16:36:37 -0500

    clean up

--- chess.py
+++ chess.py
@@ -6,82 +6,75 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
-#vegetable = driver.find_element(By.CLASS_NAME, "tomatoes")
 url = driver.command_executor._url       #"http://127.0.0.1:60622/hub"
 session_id = driver.session_id            #'4e167f26-dc1d-4f51-a207-f761eaf73c31'
 chess_url = "https://www.chess.com/play/online"
 driver.get(chess_url)
 
 
-change_time_button = driver.find_element(By.CLASS_NAME, "icon-font-chess.chevron-bottom.time-selector-chevron")
-change_time_button.click()
+def change_to_bullet():
+    change_time_button = driver.find_element(By.CLASS_NAME, "icon-font-chess.chevron-bottom.time-selector-chevron")
+    change_time_button.click()
+    bullet_button = driver.find_element(By.CLASS_NAME, "ui_v5-button-component.ui_v5-button-basic.ui_v5-button-small.ui_v5-button-full.time-selector-category-btn")
+    bullet_button.click()
 
-bullet_button = driver.find_element(By.CLASS_NAME, "ui_v5-button-component.ui_v5-button-basic.ui_v5-button-small.ui_v5-button-full.time-selector-category-btn")
-bullet_button.click()
+def find_match():
 
-play_button = driver.find_element(By.CLASS_NAME, "ui_v5-button-component.ui_v5-button-primary.ui_v5-button-large.ui_v5-button-full")
-play_button.click()
+    # click play
+    play_button = driver.find_element(By.CLASS_NAME, "ui_v5-button-component.ui_v5-button-primary.ui_v5-button-large.ui_v5-button-full")
+    play_button.click()
 
-loading = True
+    # click play as guest
+    guest_button = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "guest-button")))
+    guest_button.click()
 
-while loading:
-    try:
-        guest_button = driver.find_element(By.ID, "guest-button")
-        guest_button.click()
-        loading = False
-    except:
-        continue
+def wait_game_start():
+    # wait for draw button to appear
+    WebDriverWait(driver, float('inf')).until(EC.presence_of_element_located((By.CLASS_NAME, "draw-button-label")))
 
-found_game = False
+def get_color():
+    color = "black"
 
-while not found_game:
-    try:
-        game_start = driver.find_element(By.CLASS_NAME, "draw-button-label")
-        print(str(game_start))
-        found_game = True
-    except:
-        time.sleep(2)
-        print("not started")
-        #print("No game found")
-
-
-color = "black"
-# bottom black
-
-found_color = False
-
-while not found_color:
-
-    try:
-        driver.find_element(By.CLASS_NAME, "clock-component.clock-black.clock-bottom.clock-live.clock-running.player-clock.clock-player-turn")
-        found_color = True
-    except:
-
+    found_color = False
+    while not found_color:
         try:
-            driver.find_element(By.CLASS_NAME, "clock-component.clock-black.clock-top.clock-live.clock-running.player-clock")
+            driver.find_element(By.CLASS_NAME, "clock-component.clock-black.clock-bottom.clock-live.clock-running.player-clock.clock-player-turn")
             found_color = True
-            color = "white"
         except:
-            continue
+            try:
+                driver.find_element(By.CLASS_NAME, "clock-component.clock-black.clock-top.clock-live.clock-running.player-clock")
+                found_color = True
+                color = "white"
+            except:
+                continue
+    
+    return color
+
+def wait_opponent_move(my_color):
+    # return text of opponent move
+    element = WebDriverWait(driver, float('inf')).until(
+        EC.presence_of_element_located((By.CLASS_NAME, my_color + ".node.selected"))
+    )
+
+    return element.text
 
 
-#color = driver.find_element(By.CLASS_NAME, "clock-component.clock-white.clock-top.clock-live.clock-player-turn.player-clock")
-print(str(color))
+
+change_to_bullet()
+find_match()
+wait_game_start()
+color = get_color()
+
+
+print("your color is :" + str(color))
 black_move = None
 white_move = None
 
 if color == "white":
     time.sleep(3)
 
-def wait_opponent_move(my_color):
 
-    element = WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.CLASS_NAME, my_color + ".node.selected"))
-    )
-
-    return element.text
-
-if color == 'white':
+if color == 'black':
     opponent_move = wait_opponent_move(color)
     print(opponent_move)
 
