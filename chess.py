9e36886e18f28c6368ed455440d829d5553b5e25
Richard Liu <richard.liu.tx@gmail.com>
2022-05-28 11:43:58 -0700

    cleanup

--- chess.py
+++ chess.py
@@ -115,29 +115,3 @@ while True:
     move_count += 1
 
     print(p2 + " move: " + p2_move)
-
-
-
-print("your color is :" + str(color))
-black_move = None
-white_move = None
-
-if color == "white":
-    time.sleep(3)
-
-
-if color == 'black':
-    opponent_move = wait_opponent_move(color)
-    print(opponent_move)
-
-move_count = 1
-most_recent_move_element = driver.find_element(by=By.XPATH, value='//div[@data-ply=' + '"' + str(move_count) + '"' + ']')
-most_recent_move_text = most_recent_move_element.text
-
-print("most recent move: " + most_recent_move_text)
-
-move_count += 2
-most_recent_move_element = driver.find_element(by=By.XPATH, value='//div[@data-ply=' + '"' + str(move_count) + '"' + ']')
-most_recent_move_text = most_recent_move_element.text
-
-print("most recent move: " + most_recent_move_text)
