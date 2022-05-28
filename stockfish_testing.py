1022960e8a602c1a987771ddaf00b64c62ee45da
Richard Liu <richard.liu.tx@gmail.com>
2022-05-28 12:37:48 -0700

    engine testing

--- stockfish_testing.py
+++ stockfish_testing.py
@@ -1,3 +1,15 @@
 from stockfish import Stockfish
+import chess.engine
+
+engine = chess.engine.SimpleEngine.popen_uci("stockfish")
 stockfish = Stockfish("/Users/richardliu/chess-automation/Stockfish-master/src/stockfish")
-print(str(stockfish.get_best_move()))
+stockfish.set_skill_level()
+
+
+print(str(stockfish.get_best_move_time(1000)))
+
+engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+
+board = chess.Board("1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - - 0 1")
+limit = chess.engine.Limit(time=1.0)
+engine.play(board, limit)
