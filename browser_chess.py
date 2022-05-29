5fca1324bb3ac4d09a9376ccc65eee657b6ee8dd
Richard Liu <richard.liu.tx@gmail.com>
2022-05-29 04:11:26 -0700

    comment for todo

--- browser_chess.py
+++ browser_chess.py
@@ -1,3 +1,5 @@
+# todo: new games, rematch, autoqueen/promotions
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
